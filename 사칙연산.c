5cbd097313fd945f2f980ae3b9b4fc89cc52edc2
yeoreumcho <c0331dufma@naver.com>
2024-10-15 00:58:37 +0900

    Add files via upload

---
+++ 사칙연산.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main() {
+
+	int A = 7;
+	int B= 3;
+	printf(A + B);
+	printf(A - B);
+	printf(A * B);
+	printf(A / B);
+	printf(A % B);
+
+	return 0;
+}
