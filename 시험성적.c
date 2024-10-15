c33c1c99f6ea633dad51c5bf0b4cc705ef7fc4d4
yeoreumcho <c0331dufma@naver.com>
2024-10-15 17:03:19 +0900

    Add files via upload

---
+++ 시험성적.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int score;
+	scanf("%d", &score);
+
+	if (score >= 90 ) {
+		printf("A\n");
+	}
+	else if (score >= 80 ) {
+		printf("B\n");
+	}
+	else if (score >= 70 ) {
+		printf("C\n");
+	}
+	else if (score >= 60) {
+		printf("D\n");
+	}
+	else {
+		printf("F\n");
+	}
+
+	
+	
+	
+	return 0;
+}
